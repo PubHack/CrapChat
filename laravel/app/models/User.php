89e31c44999830c9ec2c270c016142362ecce0b2
Alasdair Smith <alasdairsmith100@gmail.com>
2014-10-07 13:51:09 +0100

    Docs

--- laravel/app/models/User.php
+++ laravel/app/models/User.php
@@ -23,6 +23,13 @@ class User extends Eloquent {
 
 	protected $fillable = ['username', 'password'];
 
+	/**
+	 * Creates a new user if not already in DB
+	 *
+	 * @param array $params
+	 * @return User
+	 * @throws Exception
+	 */
 	public static function make(array $params)
 	{
 		if (User::where('username', $params['username'])->count())
@@ -44,10 +51,33 @@ class User extends Eloquent {
 		return $user;
 	}
 
+	/**
+	 * Generates a random 4 digit pin number
+	 *
+	 * @return string
+	 */
 	protected static function generatePin()
 	{
 		$pool = '0123456789';
 		return substr(str_shuffle(str_repeat($pool, 5)), 0, 4);
+	}
+
+	/**
+	 * Find a user by their pin number
+	 *
+	 * @param string $pin
+	 * @return User|bool
+	 */
+	public static function findByPin($pin)
+	{
+		$user = User::where('pin', $pin)->get();
+
+		if (count($user) <= 0)
+		{
+			return false;
+		}
+
+		return $user;
 	}
 
 }
