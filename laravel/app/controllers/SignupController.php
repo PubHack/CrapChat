2ba5ac256d3fa1fe0cdd885610e6313c09d1e142
Alasdair Smith <alasdairsmith100@gmail.com>
2014-10-07 12:24:48 +0100

    Basic form on /signup

---
+++ laravel/app/controllers/SignupController.php
@@ -0,0 +1,10 @@
+<?php
+
+class SignupController extends BaseController {
+
+	public function index()
+	{
+		return View::make('signup');
+	}
+
+}
---
+++ laravel/app/views/signup.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Crapchat</title>
+</head>
+<body>
+	<h1>Sign up</h1>
+	<form action="/signup" method="post">
+		<input type="text" name="username" placeholder="username">
+		<input type="password" name="password" placeholder="pasword">
+	</form>
+</body>
+</html>
